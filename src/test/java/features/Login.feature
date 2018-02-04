5cb0508ba688e1ac381e5404bd0725d14bbf60f6
Gaurav Singh <gaurav.singh55@gmail.com>
2018-02-04 14:40:56 +0530

    Example of data table usage added.

--- src/test/java/features/Login.feature
+++ src/test/java/features/Login.feature
@@ -1,6 +1,8 @@
 Feature: Login test
   Scenario: Successful login to app
     Given i navigate to app URL
-    And i enter username as "student"
-    And i enter password as "MaharaDemo"
+    And i enter below login details
+      | username  | password  |
+      | student   | MaharaDemo|
+    And i click login button
     Then i should see home page
--- src/test/java/steps/Login.java
+++ src/test/java/steps/Login.java
@@ -1,8 +1,13 @@
 package steps;
 
+import cucumber.api.DataTable;
+import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Login {
     @Given("^i navigate to app URL$")
@@ -10,20 +15,39 @@ public class Login {
         System.out.println("i navigate to app URL");
     }
 
-    @And("^i enter username as \"([^\"]*)\"$")
-    public void iEnterUsernameAs(String username) {
-        System.out.println("i enter username as " + username);
-    }
-
-    @And("^i enter password as \"([^\"]*)\"$")
-    public void iEnterPasswordAs(String password) {
-        System.out.println("i enter password as " + password);
-    }
-
     @Then("^i should see home page$")
     public void iShouldSeeHomePage() {
         System.out.println("i should see home page");
     }
 
+    @And("^i enter below login details$")
+    public void iEnterBelowLoginDetails(DataTable table) {
 
+        /*List<List<String>> data = table.raw();
+
+        System.out.println("Value of (0,0) is" + data.get(0).get(0));
+        System.out.println("Value of (0,1) is " + data.get(0).get(1));*/
+
+        List<User> users = table.asList(User.class);
+
+        for (User user: users) {
+            System.out.println("Username is: " + user.username);
+            System.out.println("Password is: " + user.password);
+        }
+    }
+
+    @And("^i click login button$")
+    public void iClickLoginButton() {
+        System.out.println("i click login button");
+    }
+
+    public class User {
+        private String username;
+        private String password;
+
+        public User(String username, String password) {
+            this.username = username;
+            this.password = password;
+        }
+    }
 }
